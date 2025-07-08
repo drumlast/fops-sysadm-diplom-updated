7bd4fdaebea0bf95126429e0a71f7bf235b0025e
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-09 00:08:06 +0300

    nat

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,7 +34,7 @@ resource "yandex_vpc_route_table" "private" {
   network_id = yandex_vpc_network.main.id
   static_route {
     destination_prefix = "0.0.0.0/0"
-    gateway_id        = yandex_vpc_gateway.internet.id
+    next_hop_address   = "10.10.1.6"
   }
 }
 
---
+++ infrastructure/web.tf
@@ -0,0 +1,29 @@
+resource "yandex_compute_instance" "web" {
+  count       = 2
+  name        = "web-${count.index + 1}"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd876gids9srs8ma0592"
+      size     = 10
+    }
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.private.id
+    nat                = false
+    security_group_ids = [yandex_vpc_security_group.web.id]
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
