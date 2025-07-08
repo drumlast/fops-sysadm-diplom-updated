b81145d2a02eb9cc5caaf5408864d5bfd464946d
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-09 00:10:39 +0300

    commit

--- infrastructure/security_groups.tf
+++ infrastructure/security_groups.tf
@@ -1,23 +1,92 @@
+resource "yandex_vpc_security_group" "bastion" {
+  name       = "bastion-sg"
+  network_id = yandex_vpc_network.main.id
+
+    ingress {
+    protocol       = "TCP"
+    description    = "Allow SSH from anywhere"
+    port           = 22
+    v4_cidr_blocks = ["188.242.84.65/32", "185.155.99.112/32"]
+  }
+
+   egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "yandex_vpc_security_group" "nat" {
+  name       = "nat-sg"
+  network_id = yandex_vpc_network.main.id
+
+  ingress {
+    protocol       = "ANY"
+    description    = "Allow all from private subnet"
+    v4_cidr_blocks = ["10.10.2.0/24"]
+    from_port      = 0
+    to_port        = 65535
+  }
+
+  ingress {
+    protocol       = "ICMP"
+    description    = "Allow ICMP from private subnet"
+    v4_cidr_blocks = ["10.10.2.0/24"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Allow SSH from my IP"
+    v4_cidr_blocks = ["188.242.84.65/32", "185.155.99.112/32"]
+    port           = 22
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "Allow all outbound"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
+resource "yandex_vpc_security_group" "web" {
+  name       = "web-sg"
+  network_id = yandex_vpc_network.main.id
+
+  ingress {
+    protocol       = "TCP"
+    description    = "Allow SSH from bastion or your IP"
+    v4_cidr_blocks = ["10.10.1.0/24", "188.242.84.65/32", "185.155.99.112/32"]
+    port           = 22
+  }
+
+  ingress {
+    protocol       = "ICMP"
+    description    = "Allow ICMP from NAT"
+    v4_cidr_blocks = ["10.10.1.0/24"]
+  }
+
+  egress {
+    protocol       = "ANY"
+    description    = "Allow all outbound"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
 resource "yandex_vpc_security_group" "alb_sg" {
   name       = "alb-sg"
   network_id = yandex_vpc_network.main.id
 
-  # Входящий трафик от well-known ranges Яндекса для health checks
   ingress {
     protocol       = "TCP"
     description    = "Allow health checks from Yandex"
     port           = 80
-    v4_cidr_blocks = [
-      "130.193.0.0/16",
-      "178.154.0.0/16"
-    ]
+    v4_cidr_blocks = ["130.193.0.0/16", "178.154.0.0/16"]
   }
 
-  # Исходящий трафик к backend-серверам (web-серверы)
   egress {
-    protocol       = "TCP"
-    description    = "Send traffic to backend web instances"
-    port           = 80
-    v4_cidr_blocks = ["10.10.2.0/24"]
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
