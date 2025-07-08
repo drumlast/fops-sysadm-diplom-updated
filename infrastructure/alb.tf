7fd48e85fd36390445a6ed723a22dd9f2111774c
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 23:57:58 +0300

    new sg and alb.tf

--- infrastructure/alb.tf
+++ infrastructure/alb.tf
@@ -1,5 +1,6 @@
 resource "yandex_alb_target_group" "web_tg" {
   name = "web-target-group"
+
   dynamic "target" {
     for_each = yandex_compute_instance.web[*]
     content {
@@ -20,6 +21,7 @@ resource "yandex_alb_backend_group" "web_bg" {
     healthcheck {
       interval = "10s"
       timeout  = "5s"
+
       http_healthcheck {
         path = "/"
       }
@@ -34,8 +36,10 @@ resource "yandex_alb_http_router" "http_router" {
 resource "yandex_alb_virtual_host" "vh" {
   name           = "web-vh"
   http_router_id = yandex_alb_http_router.http_router.id
+
   route {
     name = "web-route"
+
     http_route {
       http_route_action {
         backend_group_id = yandex_alb_backend_group.web_bg.id
@@ -47,6 +51,7 @@ resource "yandex_alb_virtual_host" "vh" {
 resource "yandex_alb_load_balancer" "alb" {
   name       = "web-alb"
   network_id = yandex_vpc_network.main.id
+
   security_group_ids = [yandex_vpc_security_group.alb_sg.id]
 
   allocation_policy {
@@ -58,12 +63,14 @@ resource "yandex_alb_load_balancer" "alb" {
 
   listener {
     name = "http-listener"
+
     endpoint {
       address {
         external_ipv4_address {}
       }
       ports = [80]
     }
+
     http {
       handler {
         http_router_id = yandex_alb_http_router.http_router.id
