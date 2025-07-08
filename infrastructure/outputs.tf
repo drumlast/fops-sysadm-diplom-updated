75f489a75039dff0ad128fb4aa01f0938988406e
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 18:30:19 +0300

    dsgf

--- infrastructure/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bastion_ip" {
-  value = yandex_compute_instance.bastion.network_interface.0.nat_ip_address
-}
-
-output "alb_ip" {
-  value = yandex_alb_load_balancer.web.listener[0].endpoint[0].address[0].external_ipv4_address
-}
