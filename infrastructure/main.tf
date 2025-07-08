15302e85c363c2d7ed345ff976eb8d07bb26f7f9
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 12:54:01 +0300

    Updated main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,7 +11,7 @@ provider "yandex" {
   token     = var.yc_token
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
-  zone      = ru-central1-a
+  zone      = "ru-central1-a"
 }
 
 # VPC
