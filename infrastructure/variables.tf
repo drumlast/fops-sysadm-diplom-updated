b478f43312c13d8981a344dfeaed27ab204f68f3
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 15:04:40 +0300

    Edited variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,3 +4,7 @@ variable "yc_folder_id" {}
 variable "image_id" {
   default = "fd876gids9srs8ma0592"
 }
+variable "public_ssh_key" {
+  description = "Публичный SSH ключ для подключения"
+  type        = string
+}
