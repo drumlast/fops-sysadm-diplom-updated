2cb2396bc71e8d50e331f6aa3299e8887eaa0061
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 23:36:14 +0300

    remove rule

--- infrastructure/security_groups.tf
+++ infrastructure/security_groups.tf
@@ -73,14 +73,6 @@ resource "yandex_vpc_security_group" "web" {
     to_port        = 65535
   }
 
-  ingress {
-  protocol       = "TCP"
-  description    = "Allow HTTP from ALB and bastion"
-  port           = 80
-  v4_cidr_blocks = ["10.10.1.0/24", "10.10.2.0/24"]
-}
-}
-
 resource "yandex_vpc_security_group" "alb_sg" {
   name       = "alb-sg"
   network_id = yandex_vpc_network.main.id
