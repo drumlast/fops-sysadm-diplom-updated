3031230a534b0db4c7dd475f54247166b602bf64
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 13:10:14 +0300

    Updated key

--- infrastructure/web.tf
+++ infrastructure/web.tf
@@ -24,6 +24,6 @@ resource "yandex_compute_instance" "web" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
   }
 }
