1fffe66f92aca871dee242c31b98f0c0b8f862df
drumlast <65900500+drumlast@users.noreply.github.com>
2025-07-08 17:40:45 +0300

    Deleted sg

--- infrastructure/security_groups.tf
+++ infrastructure/security_groups.tf
@@ -80,23 +80,3 @@ resource "yandex_vpc_security_group" "web" {
   v4_cidr_blocks = ["10.10.1.0/24", "10.10.2.0/24"]
 }
 }
-
-resource "yandex_vpc_security_group" "alb" {
-  name       = "alb-sg-renamed"
-  network_id = yandex_vpc_network.main.id
-
-  ingress {
-    protocol       = "TCP"
-    description    = "Allow HTTP from Yandex Health Checks"
-    port           = 80
-    v4_cidr_blocks = ["0.0.0.0/0", "0.0.0.0/0"]
-  }
-
-  egress {
-    protocol       = "ANY"
-    description    = "Allow all outbound"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 0
-    to_port        = 65535
-  }
-}
